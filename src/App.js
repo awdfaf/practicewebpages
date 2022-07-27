96d27cb9906b0f40bb4d8c89b4e588dcd051b953
awdfaf <awdfaf@kakao.com>
2022-07-27 22:50:06 +0900

    홈페이지 제작

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import HomePage from './pages/HomePage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage></HomePage>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
