faea2e88127aabb6fe3ff9d85ee379bb2d1b2689
awdfaf <awdfaf@kakao.com>
2022-08-02 18:31:09 +0900

    수정3

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -36,18 +36,18 @@ function Article(props){
   </article>
 }
 
-function SplitPane(props) {
-  return (
-    <div className="SplitPane">
-      <div className="SplitPane-left">
-        {props.left}
-      </div>
-      <div className="SplitPane-right">
-        {props.right}
-      </div>
-    </div>
-  );
-};
+// function SplitPane(props) {
+//   return (
+//     <div className="SplitPane">
+//       <div className="SplitPane-left">
+//         {props.left}
+//       </div>
+//       <div className="SplitPane-right">
+//         {props.right}
+//       </div>
+//     </div>
+//   );
+// };
 
 function Create(props){
   return <article>
